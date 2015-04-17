1fa45176173fec31ef29e5a27ff0be6f6297f67e
John Kinney <johnkinney77@gmail.com>
2015-04-17 11:05:39 -0400

    fixed my schema issue this morning by changing a couple value names, and adding a few. Was able to finish the with making the last updated article portion work. Up to making the changelog

--- app.js
+++ app.js
@@ -19,10 +19,22 @@ app.use(express.static('./static'));
 
 app.get('/', function (req, res) {
 	var template = fs.readFileSync('./views/index.html', 'utf8');
-  db.all('SELECT ID, first_name, last_name, AID, title, date_made FROM users, articles WHERE users.ID = articles.user_id', function (error, articles) {
+  db.all('SELECT ID, first_name_author, last_name_author, AID, title, date_made FROM users, articles WHERE users.ID = articles.author_id', function (error, articles) {
       var html = mustache.render(template, {allArticles: articles});
       res.send(html);
   });
+});
+
+app.get('/sign-up', function (req, res) {
+  var html = fs.readFileSync('./views/signup.html', 'utf8');
+  res.send(html)
+});
+
+app.post('/sign-up', function (req, res) {
+  var user = req.body
+
+  db.run("INSERT INTO users (ID, first_name_author, last_name_author, email) VALUES (null, '" + user.first_name + "', '" + user.last_name + "', '" + user.email + "');");
+  res.redirect('/');
 });
 
 app.get('/article/new', function (req, res) {
@@ -38,12 +50,14 @@ app.post('/article/create', function (req, res) {
   var articleInfo = req.body;
   var author = req.body.author.split(' ');
 
-  db.all("SELECT ID FROM users WHERE first_name='" + author[0] + "' AND last_name='" + author[1] + "';", function (error, userID) {
-    console.log(userID);
-    db.run("INSERT INTO articles (AID, title, date_made, image, user_id, content, category) VALUES (null, '" + articleInfo.title +"', '" + date + "', '" + articleInfo.url + "', " + userID[0].ID + ", '" + articleInfo.content + "', '" + articleInfo.category + "');");
+  db.all("SELECT ID FROM users WHERE first_name_author='" + author[0] + "' AND last_name_author='" + author[1] + "';", function (error, authorID) {
+    
+    console.log(authorID);
+
+    db.run("INSERT INTO articles (AID, title, date_made, image, author_id, content, category) VALUES (null, '" + articleInfo.title +"', '" + date + "', '" + articleInfo.url + "', " + authorID[0].ID + ", '" + articleInfo.content + "', '" + articleInfo.category + "');");
 
     db.all("SELECT AID FROM articles WHERE title='" + articleInfo.title + "';", function (error, articleID) {
-      db.run("INSERT INTO changelog (CID, article_id, user_id, content_update, date_update) VALUES (null, " + articleID[0].AID + ", " + userID[0].ID + ", '" +  articleInfo.content + "', '" + date + "');");
+      db.run("INSERT INTO changelog (CID, first_name_editor, last_name_editor, content_update, date_update, article_id,editor_id) VALUES (null, '" + author[0] + "', '" + author[1] + "', '" + articleInfo.content + "', '" + date + "', " +articleID[0].AID + ", " + authorID[0].ID + ");");
 
       res.redirect('/' + articleInfo.title);
     });
@@ -53,21 +67,52 @@ app.post('/article/create', function (req, res) {
 app.get('/:articleName', function (req, res) {
   var title = req.params.articleName;
   var template = fs.readFileSync('./views/individualArticle.html', 'utf8');
-  db.all("SELECT ID, first_name, last_name, email, AID, title, date_made, image, content, category, CID, date_update, content_update FROM users, articles, changelog WHERE users.ID = articles.user_id AND articles.AID = changelog.article_id AND articles.title='" + title + "';", function (error, individualArticle) {
-      console.log(individualArticle)
-      var html = mustache.render(template, individualArticle[0]);
+  db.all("SELECT * FROM users, articles, changelog WHERE users.ID = articles.author_id AND articles.AID = changelog.article_id AND articles.title='" + title + "';", function (error, individualArticle) {
+      var length = individualArticle.length - 1
+      var html = mustache.render(template, individualArticle[length]);
       res.send(html);
   });
 });
 
+/*ID, first_name_author, last_name_author, email, AID, title, date_made, image, content, category, CID, date_update, content_update*/
+
 app.get('/:articleName/edit', function (req, res) {
   var title = req.params.articleName;
   var template = fs.readFileSync('./views/editArticle.html', 'utf8');
-  db.all("SELECT * FROM articles, users WHERE title='" + title +"' AND user_id = ID;", function (error, data) {
-    var html = mustache.render(template, data[0]);
-    res.send(data);
-  });
+  db.all("SELECT * FROM users, articles, changelog WHERE title='" + title +"' AND articles.author_id = ID AND changelog.article_ID = AID;", function (error, data) {
+    /*var inbetween = mustache.render(template, data[0]);
+    console.log(inbetween)*/
+    db.all("SELECT first_name_author, last_name_author FROM users", function (error, author) {
+      data[0].list = author
+      var html = mustache.render(template, data[0]);
+      res.send(html);
 
+    });
+  });
+});
+
+app.post('/:articleName/save', function (req, res) {
+    var body = req.body;
+    var date = new Date().toString().split(' ').splice(0, 5).join(' ');
+    var author = body.author.split(' ');
+    db.all("SELECT AID FROM articles WHERE title='" + body.title + "';", function (error, articleID) {
+      db.all("SELECT ID FROM users WHERE first_name_author='" + author[0] + "' AND last_name_author='" + author[1] + "';", function (error, userID) { 
+        db.run("INSERT INTO changelog (CID, first_name_editor, last_name_editor, content_update, date_update, article_id, editor_id) VALUES (null, '" + author[0] + "', '" + author[1] + "', '" + body.content_update + "', '" + date + "', " + articleID[0].AID + ", " + userID[0].ID + ");");
+        db.run("UPDATE articles SET content='" + body.content_update + "' WHERE AID=" + articleID[0].AID + ";")
+        res.redirect('/' + body.title);
+      });
+    });
+});
+
+app.get('/:articleName/changelog', function (req, res) {
+  var title = req.params.articleName;
+  db.all("SELECT * FROM articles WHERE title='" + title + "';", function (error, articleData) {
+    var AID = articleData[0].AID
+    db.all("SELECT * FROM changelog WHERE article_id=" + AID + ";", function (error, changelogData) {
+      articleData[0].changelog = changelogData;
+    res.send(articleData);
+    });
+  });
 });
 
 
--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('./workwiki.db');
 
-db.run("INSERT INTO users (ID, first_name, last_name, email) VALUES (null, 'John', 'Kinney', 'johnkinney77@gmail.com');");
-db.run("INSERT INTO users (ID, first_name, last_name, email) VALUES (null, 'Gabby', 'Losch', 'nonsense@gmail.com');")
+db.run("INSERT INTO users (ID, first_name_author, last_name_author, email) VALUES (null, 'John', 'Kinney', 'johnkinney77@gmail.com');");
+db.run("INSERT INTO users (ID, first_name_author, last_name_author, email) VALUES (null, 'Gabby', 'Losch', 'nonsense@gmail.com');")
 
-db.run("INSERT INTO articles (AID, title, date_made, image, user_id, content, category) VALUES (null, 'First Article', 'Wed Apr 15 2015', 'http://petattack.com/wp-content/uploads/2014/07/little_cute_cat_1920x1080.jpg', 1, 'Lorem ipsum dolor sit amet.','Cats');");
-db.run("INSERT INTO articles (AID, title, date_made, image, user_id, content, category) VALUES (null, 'Second Article', 'Wed Apr 16 2015', 'http://thumbs.media.smithsonianmag.com//filer/cf/68/cf688a05-ac28-4770-ae69-92079b8c23cf/05_15_2014_darwin.jpg__800x600_q85_crop_subject_location-1699,2235.jpg', 2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent auctor placerat turpis.', 'Beards');")
-
-
+db.run("INSERT INTO articles (AID, title, date_made, image, author_id, content, category) VALUES (null, 'First Article', 'Wed Apr 15 2015', 'http://petattack.com/wp-content/uploads/2014/07/little_cute_cat_1920x1080.jpg', 1, 'Lorem ipsum dolor sit amet.','Cats');");
+db.run("INSERT INTO articles (AID, title, date_made, image, author_id, content, category) VALUES (null, 'Second Article', 'Wed Apr 16 2015', 'http://thumbs.media.smithsonianmag.com//filer/cf/68/cf688a05-ac28-4770-ae69-92079b8c23cf/05_15_2014_darwin.jpg__800x600_q85_crop_subject_location-1699,2235.jpg', 2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent auctor placerat turpis.', 'Beards');")
 
 
-db.run("INSERT INTO changelog (CID, article_id, user_id, content_update, date_update) VALUES (null, 1, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent auctor placerat turpis, vitae aliquam est volutpat non. Suspendisse in posuere orci, nec pretium orci. Vivamus fringilla, lorem sed imperdiet mattis.','Wed Apr 15 2015');");
-db.run("INSERT INTO changelog (CID, article_id, user_id, content_update, date_update) VALUES (null, 2, 2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent auctor placerat turpis.','Wed Apr 16 2015');");
+
+
+db.run("INSERT INTO changelog (CID, first_name_editor, last_name_editor, content_update, date_update, article_id, editor_id) VALUES (null, 'John', 'Kinney', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent auctor placerat turpis, vitae aliquam est volutpat non. Suspendisse in posuere orci, nec pretium orci. Vivamus fringilla, lorem sed imperdiet mattis.','Wed Apr 15 2015', 2, 1);");
+db.run("INSERT INTO changelog (CID, first_name_editor, last_name_editor, content_update, date_update, article_id, editor_id) VALUES (null, 'Gabby', 'Losch', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent auctor placerat turpis, vitae aliquam est volutpat non. Suspendisse in posuere orci, nec pretium orci. Vivamus fringilla, lorem sed imperdiet mattis.','Wed Apr 15 2015', 1, 2);");
